598ab414b0411ecf1c1d74702d123341d25f7f54
AlexaV2002 <alexavakulenko@yandex.ru>
2021-09-22 17:32:55 +0300

    test commit

---
+++ src/ru/mirea/task2/Shape.java
@@ -0,0 +1,24 @@
+package ru.mirea.task2;
+
+public class Shape {
+    String name;
+    public Shape(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "name of figure= " + name;
+    }
+}
---
+++ src/ru/mirea/task2/ShapeTest.java
@@ -0,0 +1,12 @@
+package ru.mirea.task2;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape squ = new Shape("Square");
+        Shape cir = new Shape("Circle");
+        Shape tri = new Shape("Triangle");
+        System.out.println(squ);
+        System.out.println(cir);
+        System.out.println(tri);
+    }
+}
