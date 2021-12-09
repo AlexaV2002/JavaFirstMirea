50877b6383f61ad537e0096f1e2c8329fe527f32
Alexandra Vakulenko <alexavakulenko@yandex.ru>
2021-12-09 10:49:04 +0300

    test commit

--- src/ru/mirea/task25/CreditAccoun.java
+++ src/ru/mirea/task25/CreditAccoun.java
@@ -1,6 +1,6 @@
 package ru.mirea.task25;
 
-public class CreditAccount extends SpecialAccount{
+class CreditAccount extends SpecialAccount{
     public CreditAccount(BankAccount bankAcc){
         super(bankAcc, "Кредитный счёт", 8);
     }
